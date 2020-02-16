daa09774337833e8880320abe9b56b7904ab56f7
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:37:30 +0100

    Adds repository's methods with coroutines to use DAO's queries

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/repository/PhotoRepository.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/repository/PhotoRepository.kt
@@ -1,13 +1,16 @@
 package ovh.geoffrey_druelle.typicodephotos.data.repository
 
+import androidx.lifecycle.LiveData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import ovh.geoffrey_druelle.typicodephotos.TypicodePhotosApp
 import ovh.geoffrey_druelle.typicodephotos.data.local.dao.PhotoDao
 import ovh.geoffrey_druelle.typicodephotos.data.local.database.TypicodePhotosDatabase
+import ovh.geoffrey_druelle.typicodephotos.data.local.model.PhotoEntity
 import kotlin.coroutines.CoroutineContext
 
-class PhotoRepository(app: TypicodePhotosApp): CoroutineScope {
+class PhotoRepository(app: TypicodePhotosApp) : CoroutineScope {
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main
@@ -18,4 +21,27 @@ class PhotoRepository(app: TypicodePhotosApp): CoroutineScope {
         val db = TypicodePhotosDatabase.getInstance(app)
         photoDao = db.photoDao()
     }
+
+    suspend fun countPhotosEntries(): Int {
+        return withContext(Dispatchers.IO) {
+            photoDao.countEntries()
+        }
+    }
+
+    suspend fun deleteAll() {
+        withContext(Dispatchers.IO) {
+            photoDao.deleteAll()
+        }
+    }
+
+    suspend fun insert(photo: PhotoEntity) {
+        withContext(Dispatchers.IO) {
+            photoDao.insert(photo)
+        }
+    }
+
+    fun getPhotosList(): LiveData<List<PhotoEntity>> {
+        return photoDao.getAll()
+    }
+
 }
