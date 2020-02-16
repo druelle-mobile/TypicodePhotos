efc80e2806992f0f3e2f660063340a0485785df8
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:38:31 +0100

    Corrects API's call method to get limited list of items

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/remote/api/TypicodeApi.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/remote/api/TypicodeApi.kt
@@ -3,9 +3,12 @@ package ovh.geoffrey_druelle.typicodephotos.data.remote.api
 import ovh.geoffrey_druelle.typicodephotos.data.remote.model.Photo
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TypicodeApi {
 
     @GET("photos")
-    fun getPhotosData() : Call<Photo>
+    fun getPhotosData(
+        @Query("_limit") limit: Int
+    ) : Call<List<Photo>>
 }
