a4c6aac1e9e388167562ce818d90ad9051ae92b5
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:36:30 +0100

    Adds queries to Dao

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/local/dao/PhotoDao.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/local/dao/PhotoDao.kt
@@ -1,10 +1,20 @@
 package ovh.geoffrey_druelle.typicodephotos.data.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
 import ovh.geoffrey_druelle.typicodephotos.core.BaseDao
 import ovh.geoffrey_druelle.typicodephotos.data.local.model.PhotoEntity
 
 @Dao
 interface PhotoDao: BaseDao<PhotoEntity> {
 
+    @Query("SELECT COUNT(*) FROM PhotoEntity")
+    fun countEntries(): Int
+
+    @Query("DELETE FROM PhotoEntity")
+    fun deleteAll()
+
+    @Query("SELECT * FROM PhotoEntity")
+    fun getAll(): LiveData<List<PhotoEntity>>
 }
