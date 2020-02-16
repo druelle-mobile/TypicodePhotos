39ee902af710755c3c9bfe0af56d4763103a96ea
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:39:17 +0100

    Replaces Int values by Long (love "id")

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/remote/model/Photo.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/remote/model/Photo.kt
@@ -3,8 +3,8 @@ package ovh.geoffrey_druelle.typicodephotos.data.remote.model
 import com.google.gson.annotations.SerializedName
 
 data class Photo(
-    @SerializedName("albumId") val albumId: Int,
-    @SerializedName("id") val id: Int,
+    @SerializedName("albumId") val albumId: Long,
+    @SerializedName("id") val id: Long,
     @SerializedName("thumbnailUrl") val thumbnailUrl: String,
     @SerializedName("title") val title: String,
     @SerializedName("url") val url: String
