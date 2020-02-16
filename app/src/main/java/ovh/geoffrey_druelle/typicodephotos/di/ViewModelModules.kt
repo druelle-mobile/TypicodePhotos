015ff340a8ebeee2d05fe92f4059a5c5b98144d8
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:42:03 +0100

    Deletes unused viewModel and add api getters

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/di/ViewModelModules.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/di/ViewModelModules.kt
@@ -1,15 +1,14 @@
 package ovh.geoffrey_druelle.typicodephotos.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import ovh.geoffrey_druelle.typicodephotos.ui.activity.MainViewModel
-import ovh.geoffrey_druelle.typicodephotos.ui.fragment.details.DetailsViewModel
 import ovh.geoffrey_druelle.typicodephotos.ui.fragment.list.ListViewModel
 import ovh.geoffrey_druelle.typicodephotos.ui.fragment.splash.SplashScreenViewModel
 
 val viewModelModules = module {
     viewModel { MainViewModel() }
-    viewModel { ListViewModel() }
-    viewModel { SplashScreenViewModel() }
-    viewModel { DetailsViewModel() }
+    viewModel { ListViewModel(api = get(named("api"))) }
+    viewModel { SplashScreenViewModel(api = get(named("api"))) }
 }
