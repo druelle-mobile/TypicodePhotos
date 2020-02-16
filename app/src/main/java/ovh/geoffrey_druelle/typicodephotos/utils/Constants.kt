9bf31b140ef8cef045ba4d4ac9802154aa6beab8
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 14:24:02 +0100

    Implements utils files

---
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/utils/Constants.kt
@@ -0,0 +1,12 @@
+package ovh.geoffrey_druelle.typicodephotos.utils
+
+// Database constants
+const val DB_NAME = "Typicode_Photos"
+
+// Network constants
+// Different timeouts for the OkHttpClient
+const val CONNECT_TIMEOUT = 15L
+const val WRITE_TIMEOUT = 15L
+const val READ_TIMEOUT = 15L
+// Base of the url of used API
+const val BASE_URL = "http://jsonplaceholder.typicode.com/"
---
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/utils/ext/ViewExtensions.kt
@@ -0,0 +1,37 @@
+package ovh.geoffrey_druelle.typicodephotos.utils.ext
+
+import android.app.Activity
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import ovh.geoffrey_druelle.typicodephotos.ui.activity.MainActivity
+
+fun View.show() {
+    visibility = VISIBLE
+}
+
+fun View.hide() {
+    visibility = GONE
+}
+
+fun hideKeyboard(view: View?) {
+    if (view == null) {
+        return
+    }
+    val activity = MainActivity.instance
+    val imm = activity.getSystemService(
+        Activity.INPUT_METHOD_SERVICE
+    ) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun hideKeyboard(activity: Activity) {
+    val imm =
+        activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    var view = activity.currentFocus
+    if (view == null) {
+        view = View(activity)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
