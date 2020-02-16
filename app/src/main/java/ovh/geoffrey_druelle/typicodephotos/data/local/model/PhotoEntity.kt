674d69cd1dd2b01a9e37e3ebc747ab133a967313
Geoffrey Druelle <druelle.mobile@gmail.com>
2020-02-16 16:40:17 +0100

    Corrects the entity class as data class and implements Parcelable

--- app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/local/model/PhotoEntity.kt
+++ app/src/main/java/ovh/geoffrey_druelle/typicodephotos/data/local/model/PhotoEntity.kt
@@ -1,16 +1,19 @@
 package ovh.geoffrey_druelle.typicodephotos.data.local.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity
-class PhotoEntity(
+data class PhotoEntity(
     @PrimaryKey(autoGenerate = false)
     var id: Long,
     var albumId: Long,
     var title: String,
     var url: String,
     var thumbnailUrl: String
-) {
+): Parcelable {
 
 }
